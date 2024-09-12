d3cfb52c11747a745b9bf29abd2b37692707caee
aroeltgen <ar329@sunyorange.edu>
2024-09-12 13:16:00 -0400

    updated a solution

--- Assignment3/assignment3.js
+++ Assignment3/assignment3.js
@@ -21,18 +21,28 @@ console.log(quotient(20, 6)); // Output: 3
 
 // Exercise 3
 
+// Write a function sumFirstNIntegers that takes the value of N as
+// its argument and returns the sum of those integers, as
+// illustrated in the following sample run:
 
 
 
 function sumFirstNIntegers(N) {
 
+let sum = 0;
+
+for (let i = 1; i <= N; i++) {
+    sum += i;
+}
 
 
 }
 
 
 
-console.log(sumFirstNIntegers(3));
+console.log(sumFirstNIntegers(3)); // 6
+console.log(sumFirstNIntegers(100)); // 5050
+
 
 
 
